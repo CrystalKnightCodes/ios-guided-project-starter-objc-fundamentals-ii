fd5597e2d644136d015b905d3baba9a256ed305a
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-21 21:53:01 -0400

    Added model file

---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Christy Hicks on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
