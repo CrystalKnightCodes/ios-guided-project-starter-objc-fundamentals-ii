742f0749b2efd99c6555e2c9afc252395ec1b2d9
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-21 22:28:57 -0400

    Fleshed out the public interface for the model controller

--- Tips/Model Controller/LSITipController.h
+++ Tips/Model Controller/LSITipController.h
@@ -8,14 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-//@class is faster than importing, but less secure. (Forward Declaration)
+//@class is faster than importing, but less secure. (Forward Declaration, can only be used in header file.)
 @class LSITip;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITipController : NSObject
 
+@property (nonatomic, readonly) NSUInteger tipCount;
 @property(nonatomic, readonly, copy) NSArray<LSITip *> *tips;
+
+- (void)addTip:(LSITip *)aTip;
+
+- (LSITip *)tipAtIndex:(NSUInteger)index;
 
 @end
 
