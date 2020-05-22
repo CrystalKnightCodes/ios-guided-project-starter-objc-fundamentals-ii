dae54eac08ce1cefc0b0e30b086dc676467f737a
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-21 22:13:57 -0400

    Defined initializer in .m Model

--- Tips/Model/LSITip.m
+++ Tips/Model/LSITip.m
@@ -9,5 +9,19 @@
 #import "LSITip.h"
 
 @implementation LSITip
-
+- (instancetype)initWithName:(NSString *)aName
+                       total:(double)aTotal
+                  splitCount:(NSInteger)aSplitCount
+               tipPercentage:(double)aTipPercentage
+{
+    if (self = [super init]) {
+        // Only use underscores in init, or when changing value of a readonly property.  Should use self.property for all others.
+        _name = aName;
+        _total = aTotal;
+        _splitCount = aSplitCount;
+        _tipPercentage = aTipPercentage;
+        
+    }
+    return self;
+}
 @end
